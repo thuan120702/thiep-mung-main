ce859bb8dfcde977b7956cb8c5692d97895748aa
Inari Okami <125949711+thuan120702@users.noreply.github.com>
2024-10-23 18:36:47 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Nhấn vào đây nè';
+const giftUrl = 'https://youtu.be/Wu8NeFXaoOc?si=-RKbMb188oF6L4iC';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Hiền sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với Hiền trong hôm nay và cả những ngày sau';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
